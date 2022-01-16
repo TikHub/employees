d8388ae28458d4e509071d80ec9b3985eb8184ec
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2022-01-16 20:29:59 +0400

    add mocked data for employee list

---
+++ src/app/shared/mocked-data/employee-list.ts
@@ -0,0 +1,23 @@
+export const employees = [
+  {
+    id: 1,
+    firstName: 'Tom',
+    lastName: 'Edwards',
+    company: 'EPAM',
+    salary: 3000,
+  },
+  {
+    id: 2,
+    firstName: 'Andre',
+    lastName: 'Jones',
+    company: 'Google',
+    salary: 2000,
+  },
+  {
+    id: 3,
+    firstName: 'Jane',
+    lastName: 'Douson',
+    company: 'Microsoft',
+    salary: 4000,
+  },
+];
